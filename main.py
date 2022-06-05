bc89947bb8de125b4c502c3d922bb03b6ad91583
Henry Tran <hentran@deloitte.com.au>
2022-06-05 20:52:31 +1000

    CBS-1088: restyle  empty template.

---
+++ main.py
@@ -0,0 +1,9 @@
+print('Hello, Panda');
+print('Hi Panda');
+name = input('Please enter your name: ');
+print('Hello ' + name);
+
+def my_function(country = "Norway"):
+  print("I am from " + country);
+
+my_function();
