ded76ea1a59a5b4ba33c18fccaed9151fe924d2d
Newbie01P <ong.hao.tranvan@gmail.com>
2022-06-13 01:57:04 +0000

    phuong

--- sosanh.py
+++ sosanh.py
@@ -3,7 +3,11 @@ a = input('Nhap so a')
 b = input('Nhap so b')
 if a > b :
     print('a lon hon b')
+    print('het if a lon hon b')
 elif a == b :
     print('a = b')
 else :
-    print('a < b')
+    print('a < b')  # in ra a lon hon b
+# tuoi tre em va nguoi gia
+if a>= 10 or a >=60 :
+    print('nguoi gia va tre em')
