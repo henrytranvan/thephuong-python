04732b8e0e0d577cbde6f017885bcd43cb28ab47
Newbie01P <ong.hao.tranvan@gmail.com>
2022-06-13 03:09:13 +0000

    p

--- bai1_if_else/giaiptbac2.py
+++ bai1_if_else/giaiptbac2.py
@@ -3,21 +3,23 @@ print('Nhập a b c')
 a = int(input('nhập a = '))
 b = int(input('nhập b = '))
 c = int(input('nhập c = '))
-if  a == 0 :
-    if b ==0 :
-        print('Pt vô nghiệm')
+def ptbac2(a,b,c) :
+    if  a == 0 :
+        if b ==0 :
+            print('Pt vô nghiệm')
+        else :
+            print("Phương trình có một nghiệm: x = ", + (-c / b))
     else :
-        print("Phương trình có một nghiệm: x = ", + (-c / b))
-else :
-    d = b*b - 4*a*c
-    if d < 0 :
-        print(' Phương trình vô nghiệm')
-    elif d == 0 :
-        nghiem =  -b/2*a
-        print('Phương trình có nghiệm là ' + str(nghiem) )
-    else :
-        x1 =(-b + math.sqrt(d))/2*a
-        x2 =(-b - math.sqrt(d))/2*a
-        print('Phương trình có 2 nghiệm' )
-        print( 'x1 =' + str(x1) )
-        print( 'x2 =' + str(x2) )
+        d = b*b - 4*a*c
+        if d < 0 :
+            print(' Phương trình vô nghiệm')
+        elif d == 0 :
+            nghiem =  -b/2*a
+            print('Phương trình có nghiệm là ' + str(nghiem) )
+        else :
+            x1 =(-b + math.sqrt(d))/2*a
+            x2 =(-b - math.sqrt(d))/2*a
+            print('Phương trình có 2 nghiệm' )
+            print( 'x1 =' + str(x1) )
+            print( 'x2 =' + str(x2) )
+ptbac2(3, 0, 1)
