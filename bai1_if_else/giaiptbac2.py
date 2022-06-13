3d4885e405a2985e22f2bb42212b797585573a1d
Newbie01P <ong.hao.tranvan@gmail.com>
2022-06-13 02:46:31 +0000

    phuong

---
+++ bai1_if_else/giaiptbac2.py
@@ -0,0 +1,17 @@
+import math
+print('Nhập a b c')
+a = int(input('nhập a = '))
+b = int(input('nhập b = '))
+c = int(input('nhập c = '))
+d = b*b - 4*a*c
+if d < 0 :
+    print(' Phương trình vô nghiệm')
+elif d == 0 :
+    nghiem =  -b/2*a
+    print('Phương trình có nghiệm là ' + str(nghiem) )
+else :
+    x1 =(-b + math.sqrt(d))/2*a
+    x2 =(-b - math.sqrt(d))/2*a
+    print('Phương trình có 2 nghiệm' )
+    print( 'x1 =' + str(x1) )
+    print( 'x2 =' + str(x2) )
