48de1d99b1e7ca8f9db81c98ef5548762a28247e
Newbie01P <ong.hao.tranvan@gmail.com>
2022-06-13 03:02:12 +0000

    ph

--- bai1_if_else/giaiptbac1.py
+++ bai1_if_else/giaiptbac1.py
@@ -1,5 +1,8 @@
 print('Nhập hai số a b')
 a = int(input('nhập a = '))
 b = int(input('nhập b = '))
-x = -b/a
-print('nghiệm là x = ' + str(x))
+if a == 0 :
+    print('Phương trình vô nghiệm')
+else :
+    x = -b/a
+    print('nghiệm là x = ' + str(x))
