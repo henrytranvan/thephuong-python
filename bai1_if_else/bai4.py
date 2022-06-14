aea442b5df81e97d86d0fd700c5661b44984b5ec
Newbie01P <ong.hao.tranvan@gmail.com>
2022-06-14 08:42:09 +0000

    ph

---
+++ bai1_if_else/bai4.py
@@ -0,0 +1,7 @@
+print('Hãy nhập bán kinh r và đường kính d ')
+r = input('Bán kính r ')
+d = input('Đường kính d ')
+bankinh = float(r)*float(r)*3.14
+duongkinh = float(d)*3.14
+print('Diện tích hình tròn là ' + str(bankinh))
+print('Chu vi hình tròn là ' + str(duongkinh))
