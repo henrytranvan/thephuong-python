b3cb8a0a738db3ea1050e4beb9b762e35c16fb7e
Newbie01P <ong.hao.tranvan@gmail.com>
2022-06-14 08:31:45 +0000

    ph

---
+++ bai1_if_else/bai3.py
@@ -0,0 +1,7 @@
+print('nhập vào 2 cạnh a,b')
+a = input('Nhập cạnh a ')
+b = input('Nhập cạnh b ')
+dientich = float(a)*float(b)
+print('Diện tích là ' + str(dientich)) 
+chuvi = (float(a)+float(b))*2
+print('Chu vi là ' + str(dientich))
