b8fbe3db0cd606db025d1bca4e4169ccd5639cd1
Newbie01P <ong.hao.tranvan@gmail.com>
2022-06-16 08:39:49 +0000

    phuong

--- bai1_if_else/nhapsolonnhat.py
+++ bai1_if_else/nhapsolonnhat.py
@@ -1,6 +1,13 @@
-# nhập 4 số a b c d tìm số lớn nhất
 print('Nhập 4 số abcd')
 a = input('Nhập a ')
 b = input('Nhập b ')
 c = input('Nhập c ')
 d = input('Nhập d ')
+x = [float(a),float(b),float(c),float(d)]
+solonhat = 0
+for i in x :
+    print(str(i))
+    if i > solonhat :
+        solonhat = i
+        print(str(solonhat))
+print('Số lớn nhất là ' + str(solonhat))
