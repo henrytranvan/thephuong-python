8f7e3895798309057984718642403c7bd324e5b8
Newbie01P <ong.hao.tranvan@gmail.com>
2022-06-17 08:09:56 +0000

    khct

--- bai1_if_else/hamsosanh.py
+++ bai1_if_else/hamsosanh.py
@@ -5,11 +5,3 @@ def sosanh(a,b) :
         return 'a bang b'
     else :
         return 'b la so lon hon' 
-
-print('nhap hai so a b')
-a = float(input('nhap so a'))
-b = float(input('nhap so b'))
-ketqua = sosanh(a, b)
-print('ket qua la ' + ketqua)
-
-
---
+++ bai1_if_else/thmodule.py
@@ -0,0 +1,7 @@
+import hamsosanh
+
+print('nhap hai so a b')
+a = float(input('nhap so a'))
+b = float(input('nhap so b'))
+ketqua = hamsosanh.sosanh(a, b)
+print('ket qua la ' + ketqua)
