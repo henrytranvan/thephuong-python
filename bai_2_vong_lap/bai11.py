bc6a19431e97ef952565470e847a003565b0c46f
Newbie01P <ong.hao.tranvan@gmail.com>
2022-06-19 08:16:55 +0000

    p

---
+++ bai_2_vong_lap/bai11.py
@@ -0,0 +1,16 @@
+print('Nhap 1 mang cac so')
+ketthuc = False
+ds = []
+while (ketthuc != True) :
+    a = input()
+    if a == 'k' :
+        break
+    ds.append(float(a))
+
+solonhat = 0
+for i in ds :
+    print(str(i))
+    if int(i) > solonhat :
+        solonhat = i
+        print(str(solonhat))
+print('Số lớn nhất là ' + str(solonhat))
