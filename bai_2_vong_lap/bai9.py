255baccbca9cb6d8402687f487dbe420c0af52c2
Newbie01P <ong.hao.tranvan@gmail.com>
2022-06-19 07:52:34 +0000

    ph

--- bai_2_vong_lap/bai9.py
+++ bai_2_vong_lap/bai9.py
@@ -1,9 +1,10 @@
 coso = int(input('Nhập cơ số : '))
 luythua = int(input('Nhập lũy thừa :'))
-ketqua = 1;
+
 if luythua == 0 : 
     print('Lũy thừa 0 của ' + str(coso) + 'bằng 1')
 else :
+    ketqua = 1;
     while (luythua!=0) :
         ketqua = ketqua*coso
         luythua = luythua-1
