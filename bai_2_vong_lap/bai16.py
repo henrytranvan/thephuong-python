d15d6ed7ec508566c8458ce6662b12bc38496838
Newbie01P <ong.hao.tranvan@gmail.com>
2022-06-22 01:55:44 +0000

    k

--- bai_2_vong_lap/bai16.py
+++ bai_2_vong_lap/bai16.py
@@ -1,5 +1,5 @@
 a = input('Nhập vào 1 số nguyên dương : ')
-b = []
-for i in range(0, len(a)-1) :
-    b.append(len(a)+1-i)
+b = ""
+for i in range(0, len(a)) :
+    b =  b + a[len(a)-1-i]
 print(b)
