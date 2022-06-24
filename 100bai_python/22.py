6876146389260618629618920fd24c366de94d01
Newbie01P <ong.hao.tranvan@gmail.com>
2022-06-24 04:11:19 +0000

    k

---
+++ 100bai_python/22.py
@@ -0,0 +1,7 @@
+from operator import itemgetter, attrgetter #  Bài  tập  Python  22  Code  by  Quantrimang.com   l = []
+while True:
+    s = input() 
+    if not s:
+        break 
+l.append(tuple(s.split(",")))
+
