a2a21868a9c58f0bdd4a3cb01e89c040069aee09
Newbie01P <ong.hao.tranvan@gmail.com>
2022-07-14 22:01:01 +0700

    new file:   100bai_python/39.py     new file:   100bai_python/40.py 	new file:   100bai_python/41.py 	new file:   100bai_python/42.py     new file:   100bai_python/43.py 	new file:   100bai_python/44.py 	new file:   100bai_python/45.py 	new file:   100bai_python/46.py 	new file:   100bai_python/47.py

---
+++ 100bai_python/39.py
@@ -0,0 +1,7 @@
+def printList(): 
+    danhsach=list() 
+    for i in range(1,21): 
+        danhsach.append(i**2) 
+    print (danhsach[5:]) 
+        
+printList()
---
+++ 100bai_python/43.py
@@ -0,0 +1,7 @@
+tp=(1,2,3,4,5,6,7,8,9,10) 
+ds=list() 
+for i in tp: 
+    if tp[i-1]%2==0: 
+        ds.append(tp[i-1]) 
+        tp2=tuple(ds) 
+print (tp2)
