643920551339614e448b9e8b4e72b54f2c4689b9
Newbie01P <ong.hao.tranvan@gmail.com>
2022-07-14 22:12:14 +0700

    new file:   100bai_python/48.PY 	new file:   100bai_python/49.py 	new file:   100bai_python/50.py

---
+++ 100bai_python/50.py
@@ -0,0 +1,7 @@
+class Vietnam (object): 
+    @staticmethod 
+    def printNationality (): 
+        print ("Vietnam") 
+VietnamVodich = Vietnam () 
+VietnamVodich.printNationality () 
+Vietnam.printNationality ()
