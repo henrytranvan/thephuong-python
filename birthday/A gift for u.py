89ccd143845ce7e4d896b52c5ec44e9b9b744d7d
Newbie10P <ong.hao.tranvan@gmail.com>
2022-10-05 13:03:57 +0700

    new file:   birthday/A gift for u.py

---
+++ birthday/A gift for u.py
@@ -0,0 +1,15 @@
+print('Món quà làm bởi anh Phương dz : )')
+print('Tặng ai đây ta.... ')
+a =input('Nhập ngày sinh bạn vô đei nào, không cách không dấu nha chỉ nhập số thui : ')
+if a == '05102008' :
+    print("Chúc cậu sinh nhật vui vẻ, tuổi mới xinh đẹp hơn và sẽ đỗ cấp 3 nha Trâm <3.")
+else : 
+    print('Sai òi nhập cả số 0 và không nhập cách hay dấu nhá, nhập số hoi')
+    b = input('Nhập lại nà : ')
+    while b == "05102008" : 
+        print("Chúc bạn sinh nhật vui vẻ, tuổi mới xinh đẹp hơn với sẽ đỗ cấp 3 nha <3.")
+        break
+    print('pai <3')
+
+
+    
