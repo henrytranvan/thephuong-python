b38ef896e40f8439a77be67de4b8710c1498cb09
Newbie01P <ong.hao.tranvan@gmail.com>
2023-03-08 11:29:26 +0000

    modified:   Tool made by myself and itn/83.py

--- Tool made by myself and itn/83.py
+++ Tool made by myself and itn/83.py
@@ -2,9 +2,24 @@
 import os
 
 print('Hôm nay Ngày Quốc tế Phụ nữ đúng không nhỉ ?')
-a = input('Đúng "yes" sai "no" nè : ')
+a = input('Đúng yes sai no nè : ')
 if a == "yes" :
     print("Bạn tên gì á ._.")
     b = input("Mình tên ")
     print('Vậy thì, chúc bạn ' + str(b) + " ngày càng xinh đẹp, giỏi giang và quan trọng nhất, thi đỗ nguyện vọng một nhé <3 " )
-    os.system('pause')
+    os.system("pause")
+elif a == "no" :
+    print('Vậy thôi chào bạn.')
+    os.system("pause")
+else :
+    while a != "yes" or "no" :
+        print('Sai òi nhập lại i')
+        e = input('Hỏi lại nè, yes hay no : ') 
+        if e == "yes" :
+                print("Bạn tên gì á ._.")
+                b = input("Mình tên ")
+                print('Vậy thì, chúc bạn ' + str(b) + " ngày càng xinh đẹp, giỏi giang và quan trọng nhất, thi đỗ nguyện vọng một nhé <3 " )
+                os.system("pause")
+                break
+        else :
+            pass
