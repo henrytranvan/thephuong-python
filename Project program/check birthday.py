ae342d312d64968602cdc8c3fa15652613db9b5c
Newbie01P <ong.hao.tranvan@gmail.com>
2023-04-30 14:15:51 +0700

    new file:   .vscode/launch.json 	modified:   Project program/check birthday.py 	modified:   a.txt

--- Project program/check birthday.py
+++ Project program/check birthday.py
@@ -6,7 +6,13 @@ checkdaymonth = now.strftime("%m/%d")
 print(checkdaymonth)
 f = open("a.txt",'r',encoding = 'utf-8')
 while True :
-    a = f.readline() 
+    a = f.readline()
     print(a)
     if a == "" :
         break
+    b = a[-1:4]
+    while True :    
+        print(b)
+        if True :
+            break  
+
