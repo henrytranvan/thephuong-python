bc1370043ea52618aa0a34962286185ed76423e6
Newbie01P <ong.hao.tranvan@gmail.com>
2023-04-30 13:35:07 +0700

    modified:   Project program/check birthday.py 	deleted:    Project program/kct.txt 	deleted:    a.py 	new file:   a.txt 	deleted:    adu.txt 	deleted:    get-pip.py

--- Project program/check birthday.py
+++ Project program/check birthday.py
@@ -1,20 +1,10 @@
 import datetime 
 
 now = datetime.datetime.now()
-checkday = now.strftime("%d")
-checkmonth = now.strftime("%m")
-if checkday == "30" :
-    print("true")
-else :
-    print("false")
-if checkmonth == "04" :
-    print("True")
-else :
-    print(False)
+checkdaymonth = now.strftime("%m/%d")
 
-print(checkday)
-print(checkmonth)
-f = open("kct.txt",'r',encoding = 'utf-8')
+print(checkdaymonth)
+f = open("a.txt",'r',encoding = 'utf-8')
 while True :
     a = f.readline() 
     print(a)
