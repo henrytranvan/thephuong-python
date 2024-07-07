e502a66b3dfd4d2f1e7317a06212a02fa735fa80
Your Name <you@example.com>
2024-07-07 22:55:22 +0700

    s

---
+++ Practice-english.py
@@ -0,0 +1,11 @@
+import sqlite3
+connect = sqlite3.connect("SATSQL.sql")
+cursor = connect.cursor()
+def data_entry(WORD,Definition) : 
+    connect.execute('INSERT INTO VC VALUES(?, ?)',(WORD,Definition))
+    connect.commit()
+def get_data() :
+    cursor.execute('SELECT * FROM VC')
+    data= cursor.fetchall()
+    for i in data :
+        print(d)
